5fcf48fe0cd0d151f695203da6b8ca9f7635683e
Abhi Reddy <abhirulz1003@gmail.com>
2023-06-09 00:40:01 -0400

    first commit

---
+++ src/main/java/com/abhisprojects/betterpokedex/BetterpokedexApplication.java
@@ -0,0 +1,13 @@
+package com.abhisprojects.betterpokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterpokedexApplication {
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(BetterpokedexApplication.class, args);
+	}
+
+}
