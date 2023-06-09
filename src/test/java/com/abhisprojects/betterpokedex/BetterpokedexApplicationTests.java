2aa2c1c7c2a016a03c9797577ccbade313e305e9
Abhi Reddy <abhirulz1003@gmail.com>
2023-06-09 00:40:01 -0400

    first commit

---
+++ src/test/java/com/abhisprojects/betterpokedex/BetterpokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhisprojects.betterpokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterpokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
